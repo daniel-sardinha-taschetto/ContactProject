57dbff1942c1ccfe7558b47373d36422eb2dd4b7
taschetto <taschetto@DESKTOP-AROVR2O>
2017-01-19 12:59:29 +1100

    Add project files.

---
+++ ContactProject/Models/Contact.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ContactProject.Models
+{
+    public class Contact
+    {
+        [Key]
+        public Int32 Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public String FirstName { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public String LastName { get; set; }
+
+        [MaxLength(20)]
+        public String NickName { get; set; }
+
+        [NotMapped]
+        public String DisplayAs
+        {
+            get
+            {
+                return FirstName + ", " + LastName + ", " + NickName;
+            }
+        }
+        //[DisplayFormat(DataFormatString = "{0:dd/MM/yyyy")]
+        //[DataType(DataType.Date)]
+        public DateTime DateOfBirth { get; set; }
+        [Required]
+        [MaxLength(20)]
+        [DataType(DataType.PhoneNumber)]
+        public String PhoneNumber { get; set; }
+    }
+}
